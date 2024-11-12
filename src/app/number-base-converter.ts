5a7f646d5a7c4d82f1ca7b9a6d5c10df12ce1419
Antti Puurunen <Antti.Puurunen@edu.savonia.fi>
2024-11-12 20:59:00 +0200

    From base ten to 2 and 4

--- src/app/number-base-converter.ts
+++ src/app/number-base-converter.ts
@@ -12,9 +12,48 @@ import {FormsModule} from '@angular/forms';
 export class NumberBaseConverter {
   title = 'Number base converter';
   numberToConvert = 0;
-  newBase = 0;
+  newBase = "";
+  fromBase = null;
+  toBase = null;
+  baseList = [
+    {name: 'two', baseNumber: 2},
+    {name: 'four', baseNumber: 4},
+    {name: 'ten', baseNumber: 10},    
+    {name: 'hex', baseNumber: 16}
+  ]
 
   convertBase() {
-    this.newBase = this.numberToConvert;
+    let conversionTable = []
+    this.newBase = "";
+    /*
+      Convert from base ten to another base
+      TODO: Implement other conversions
+    */
+    if (this.numberToConvert > 0 && this.fromBase != null && this.toBase != null && this.fromBase != this.toBase) {
+      // Round down to get only whole numbers
+      conversionTable.push({quotient: Math.floor(this.numberToConvert / this.toBase),
+                            remainder: (this.numberToConvert - (Math.floor(this.numberToConvert / this.toBase) * this.toBase))
+      });
+
+      // Check the last element of the array's quotient part
+      while (conversionTable[conversionTable.length - 1].quotient > 0) {
+        /* 
+           Divide with the wanted base until the non-remainder part is less than one
+           Keep track of the non-remainder part and the remainder of each step
+           If the quotient is between zero and one, the last remainder is one
+        */
+        
+        conversionTable.push({quotient: Math.floor(conversionTable[conversionTable.length - 1].quotient / this.toBase), 
+                              remainder: conversionTable[conversionTable.length - 1].quotient / this.toBase < 1 && 
+                              conversionTable[conversionTable.length - 1].quotient / this.toBase > 0 ? 
+                              1 : conversionTable[conversionTable.length - 1].quotient % (this.toBase * (Math.floor(conversionTable[conversionTable.length - 1].quotient / this.toBase)))})
+      } 
+    }
+    console.log(conversionTable)
+
+    // The number in the new base is formed from the remainders starting from the end of the array to the beginning
+    for (let i = conversionTable.length - 1; i >= 0; i--) {
+      this.newBase += conversionTable[i].remainder;
+    }
   }
 }
